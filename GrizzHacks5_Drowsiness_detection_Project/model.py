8c8c87141bf0cb081ede91dca7bf17713f4926c2
Restyled.io <commits@restyled.io>
2020-09-19 12:27:12 +0000

    Restyled by isort

--- GrizzHacks5_Drowsiness_detection_Project/model.py
+++ GrizzHacks5_Drowsiness_detection_Project/model.py
@@ -1,20 +1,14 @@
 import os
-from keras.preprocessing import image
-import matplotlib.pyplot as plt
-import numpy as np
-from keras.utils.np_utils import to_categorical
 import random
 import shutil
-from keras.models import Sequential
-from keras.layers import (
-    Dropout,
-    Conv2D,
-    Flatten,
-    Dense,
-    MaxPooling2D,
-    BatchNormalization,
-)
-from keras.models import load_model
+
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.layers import (BatchNormalization, Conv2D, Dense, Dropout, Flatten,
+                          MaxPooling2D)
+from keras.models import Sequential, load_model
+from keras.preprocessing import image
+from keras.utils.np_utils import to_categorical
 
 
 def generator(
