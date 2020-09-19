6a3cc8eecb5d51333ba654e800ea4a93d459c61d
Restyled.io <commits@restyled.io>
2020-09-19 12:27:15 +0000

    Restyled by yapf

--- GrizzHacks5_Drowsiness_detection_Project/drowsiness_detection.py
+++ GrizzHacks5_Drowsiness_detection_Project/drowsiness_detection.py
@@ -9,10 +9,12 @@ from pygame import mixer
 mixer.init()
 sound = mixer.Sound("alarm.wav")
 
-face = cv2.CascadeClassifier("haar cascade files\haarcascade_frontalface_alt.xml")
-leye = cv2.CascadeClassifier("haar cascade files\haarcascade_lefteye_2splits.xml")
-reye = cv2.CascadeClassifier("haar cascade files\haarcascade_righteye_2splits.xml")
-
+face = cv2.CascadeClassifier(
+    "haar cascade files\haarcascade_frontalface_alt.xml")
+leye = cv2.CascadeClassifier(
+    "haar cascade files\haarcascade_lefteye_2splits.xml")
+reye = cv2.CascadeClassifier(
+    "haar cascade files\haarcascade_righteye_2splits.xml")
 
 lbl = ["Close", "Open"]
 
@@ -32,21 +34,21 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face.detectMultiScale(
-        gray, minNeighbors=5, scaleFactor=1.1, minSize=(25, 25)
-    )
+    faces = face.detectMultiScale(gray,
+                                  minNeighbors=5,
+                                  scaleFactor=1.1,
+                                  minSize=(25, 25))
     left_eye = leye.detectMultiScale(gray)
     right_eye = reye.detectMultiScale(gray)
 
-    cv2.rectangle(
-        frame, (0, height - 50), (270, height), (0, 0, 0), thickness=cv2.FILLED
-    )
+    cv2.rectangle(frame, (0, height - 50), (270, height), (0, 0, 0),
+                  thickness=cv2.FILLED)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (100, 100, 100), 1)
 
     for (x, y, w, h) in right_eye:
-        r_eye = frame[y : y + h, x : x + w]
+        r_eye = frame[y:y + h, x:x + w]
         count = count + 1
         r_eye = cv2.cvtColor(r_eye, cv2.COLOR_BGR2GRAY)
         r_eye = cv2.resize(r_eye, (24, 24))
@@ -61,7 +63,7 @@ while True:
         break
 
     for (x, y, w, h) in left_eye:
-        l_eye = frame[y : y + h, x : x + w]
+        l_eye = frame[y:y + h, x:x + w]
         count = count + 1
         l_eye = cv2.cvtColor(l_eye, cv2.COLOR_BGR2GRAY)
         l_eye = cv2.resize(l_eye, (24, 24))
@@ -90,9 +92,8 @@ while True:
 
     elif rpred == 1 and rpred == 1:
         score = -1
-        cv2.putText(
-            frame, "Safe", (10, height - 20), font, 1, (255, 255, 255), 1, cv2.LINE_AA
-        )
+        cv2.putText(frame, "Safe", (10, height - 20), font, 1, (255, 255, 255),
+                    1, cv2.LINE_AA)
 
     if score < 0:
         score = 0
